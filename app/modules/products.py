70e174acf14b2e9c6d121e160419cd9f1dbe5ec0
Denis <134041882+Denis0790@users.noreply.github.com>
2025-05-09 22:31:57 +0300

    Создание таблиц в sqlalchemy

---
+++ app/modules/products.py
@@ -0,0 +1,17 @@
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, ForeignKey, Boolean, String
+from sqlalchemy.orm import relationship
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    slug = Column(String, unique=True, index=True)
+    description = Column(String)
+    price = Column(Integer)
+    image_url = Column(String)
+    stock = Column(Integer)
+    rating = Column(Integer)
+    is_active = Column(Boolean, default=True)
